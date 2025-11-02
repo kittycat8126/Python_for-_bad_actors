d0213ee87a686b5f09df30e788c6760c6defb330
redhat <redhatonyou@gmail.com>
2025-11-03 02:05:24 +0530

    day3

--- functions.py
+++ functions.py
@@ -1,4 +1,14 @@
 def myfun():
-    print("yoyo")
+    #do this 
+    print("yo")
 
-myfun()
+num = int(input("enter the number : "))
+#fizbuzz with fucntions;
+def fizzbuzz(num):
+    num = int(input("Enter the number : "))
+    if(num%15==0):
+        print("fizzbuzz")
+    elif(num%3==0):
+        print("fizz")
+    elif(num%5==0):
+        print("buzz")
